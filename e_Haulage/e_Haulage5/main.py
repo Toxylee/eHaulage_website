afb519eca0f073f2e68e2d050b1354ac6ab2fa6b
Toxylee <toxylee20021@gmail.com>
2023-09-11 19:10:23 +0000

    clean up

--- e_Haulage/e_Haulage5/main.py
+++
@@ -1,9 +0,0 @@
-# Import the create_app function from the 'website' package.
-from website import create_app
-
-# Create a Flask application instance using the create_app function.
-app = create_app()
-
-# Check if this script is being run as the main program.
-if __name__ == '__main__':
-    app.run(debug=True)
